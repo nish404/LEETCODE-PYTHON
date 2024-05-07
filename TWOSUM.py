ada92874b65f936f02f82b864bea138a339eb3fa
nish404 <77575013+nish404@users.noreply.github.com>
2024-05-07 18:31:36 -0400

    Update TWOSUM.py

--- TWOSUM.py
+++ TWOSUM.py
@@ -1,5 +1,10 @@
+'''
+GIVEN AN ARRAY OF INTEGERS NUMS AND AN INTEGER TARGET, RETURN INDICES OF THE TWO NUMBERS 
+SUCH THAT THEY ADD UP TO TARGET.
+
 Input: nums = [2, 7, 11, 15], target = 9
 Output: [0, 1] (because nums[0] + nums[1] = 2 + 7 = 9)
+'''
 
 def two_sum(nums, target):
     hashmap = {}
