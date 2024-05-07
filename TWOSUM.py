a3af07afea1574acc74ddbb216ea3b2fb8d26687
nish404 <77575013+nish404@users.noreply.github.com>
2024-05-07 18:18:22 -0400

    Create TWOSUM.py

---
+++ TWOSUM.py
@@ -0,0 +1,11 @@
+Input: nums = [2, 7, 11, 15], target = 9
+Output: [0, 1] (because nums[0] + nums[1] = 2 + 7 = 9)
+
+def two_sum(nums, target):
+    hashmap = {}
+    for i, num in enumerate(nums):
+        complement = target - num
+        if complement in hashmap:
+            return [hashmap[complement], i]
+        hashmap[num] = i
+    raise ValueError("No two sum solution")
