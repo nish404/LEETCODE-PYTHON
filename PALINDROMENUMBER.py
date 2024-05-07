a516043f81d3fdee81f47a27a5d9f8fc90773934
nish404 <77575013+nish404@users.noreply.github.com>
2024-05-07 18:21:04 -0400

    Create PALINDROMENUMBER.py

---
+++ PALINDROMENUMBER.py
@@ -0,0 +1,13 @@
+# DETERMINE WHETHER AN INTEGER IS A PALINDROME. AN INTEGER IS A PALINDROME WHEN IT READS THE SAME BACKWARDS AS FORWARD.
+
+Input: 121
+Output: true
+
+def is_palindrome(x):
+    if x < 0 or (x % 10 == 0 and x != 0):
+        return False
+    rev = 0
+    while x > rev:
+        rev = rev * 10 + x % 10
+        x //= 10
+    return x == rev or x == rev // 10
