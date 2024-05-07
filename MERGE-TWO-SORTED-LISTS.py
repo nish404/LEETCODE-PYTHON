d2bf60e6862ca4b02ab3d793adc08495842792c9
nish404 <77575013+nish404@users.noreply.github.com>
2024-05-07 18:36:45 -0400

    Create MERGE-TWO-SORTED-LISTS.py

---
+++ MERGE-TWO-SORTED-LISTS.py
@@ -0,0 +1,25 @@
+'''
+MERGE TWO SORTED LINKED LISTS AND RETURN IT AS A NEW SORTED LIST 
+
+Input: l1 = 1->2->4, l2 = 1->3->4
+Output: 1->1->2->3->4->4
+'''
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def merge_two_lists(l1, l2):
+    dummy = ListNode()
+    current = dummy
+    while l1 and l2:
+        if l1.val < l2.val:
+            current.next = l1
+            l1 = l1.next
+        else:
+            current.next = l2
+            l2 = l2.next
+        current = current.next
+    current.next = l1 if l1 else l2
+    return dummy.next
